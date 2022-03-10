a13b7a6a684774f326825e93d65ccdb9bd0a3fda
jiangtaosun <jiangtaosun@microsoft.com>
2022-03-10 13:57:34 +0800

    customize the admin form 2

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,6 +4,8 @@ from .models import Question
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
-
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 admin.site.register(Question, QuestionAdmin)
