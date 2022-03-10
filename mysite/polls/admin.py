7ecd39f50d85094ba5336771d3cd16d31698912d
jiangtaosun <jiangtaosun@microsoft.com>
2022-03-10 14:05:46 +0800

    customize the admin form - Add choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question,Choice
 
 # Register your models here.
 
@@ -9,3 +9,6 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
 admin.site.register(Question, QuestionAdmin)
+
+
+admin.site.register(Choice)
