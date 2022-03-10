4175302ba6dc30338897ce12cf6b0381315fa761
Jiangtao Sun <jiangtaosun@microsoft.com>
2022-03-10 22:01:05 +0800

    session and logout

--- mysite/login/views.py
+++ mysite/login/views.py
@@ -9,6 +9,9 @@ def index(request):
 
 
 def login(request):
+    if request.session.get('is_login', None):
+        return redirect('/index')
+
     if request.method == "POST":
         #username = request.POST.get('username')
         #password = request.POST.get('password')
@@ -20,6 +23,9 @@ def login(request):
             try:
                 user = User.objects.get(name=username)
                 if user.password == password:
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.name
                     return redirect('/index')
                 else:
                     message = '密码错误'
@@ -36,5 +42,12 @@ def register(request):
 
 
 def logout(request):
-    pass
-    return redirect('/index/')
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/index/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
+    return redirect("/index/")
